3839948f984baa2998d0524c875114239f3d92f4
AB-FlatCode <info@flatcode.hr>
2020-12-30 12:37:41 +0100

    Create post redirect completed

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:8080"
 
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
@@ -9,16 +11,34 @@ import Home from "./components/Home"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
+import CreatePost from "./components/CreatePost"
+import ViewSinglePost from "./components/ViewSinglePost"
+import FlashMessages from "./components/FlashMessages"
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("appToken")))
+  const [flashMessages, setFlashMessages] = useState([])
+
+  function addFlashMessage(msg) {
+    setFlashMessages((prev) => prev.concat(msg))
+  }
+
   return (
     <BrowserRouter>
+      <FlashMessages messages={flashMessages} />
       <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
 
       <Switch>
         <Route path="/" exact>
           {loggedIn ? <Home /> : <HomeGuest />}
+        </Route>
+
+        <Route path="/post/:id">
+          <ViewSinglePost />
+        </Route>
+
+        <Route path="/create-post">
+          <CreatePost addFlashMessage={addFlashMessage}></CreatePost>
         </Route>
 
         <Route path="/about-us">
